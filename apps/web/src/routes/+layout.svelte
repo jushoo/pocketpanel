f4dc6775263413dba9e0c6c02b079436e79b40c9
jushoo <denniel.joshua@pm.me>
2026-03-21 13:50:41 +0800

    feat: add dark mode toggle using mode-watcher

--- apps/web/src/routes/+layout.svelte
+++ apps/web/src/routes/+layout.svelte
@@ -1,9 +1,17 @@
 <script lang="ts">
 	import './layout.css';
 	import favicon from '$lib/assets/favicon.svg';
+	import { ModeWatcher } from 'mode-watcher';
+	import ModeToggle from '$lib/components/mode-toggle.svelte';
 
 	let { children } = $props();
 </script>
+
+<ModeWatcher />
+
+<div class="fixed top-4 right-4 z-50">
+	<ModeToggle />
+</div>
 
 <svelte:head>
 	<link rel="icon" href={favicon} />
